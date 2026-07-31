cd5f9087937e256b00ed73392173314f8d047f0f
Kim Bum Jun <admin@vivabj.com>
2026-07-31 11:37:24 +0900

    feat:completed hanoi tower recursive demo

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="${0:A:h}"
 PROJECT_ROOT="${SCRIPT_DIR}/Yana"
 SCRIPTS="${PROJECT_ROOT}/scripts"
 
-"${SCRIPTS}/clean.sh" && "${SCRIPTS}/build.sh"
+"${SCRIPTS}/clean.sh" && "${SCRIPTS}/build-unix.sh"
