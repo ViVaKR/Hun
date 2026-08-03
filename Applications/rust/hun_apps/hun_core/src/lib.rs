43d68061d9c0b2818585baee59b99d7922328f3a
Kim Bum Jun <admin@vivabj.com>
2026-08-03 22:58:08 +0900

    feat(rust):sys_print_fmt, 동적배열 파라미터 출력 러스트라이브러리 추가

--- Applications/rust/hun_apps/hun_core/src/lib.rs
+++ Applications/rust/hun_apps/hun_core/src/lib.rs
@@ -5,6 +5,8 @@ use std::ffi::CStr;
 use std::io::{self, Write};
 use std::os::raw::c_char;
 
+pub mod console;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn calc_sum(a: u64, b: u64) {
     println!("{} + {} = {}", a, b, a + b);
